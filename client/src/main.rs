eb539e4109ef1d6c40ee3eaf70661f714c64a24e
Eric <jhonjhonjumaday@gmail.com>
2023-11-04 14:20:23 +0800

    testing the driver module

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>>{
+    let mut minase =
+        minase_driver::Minase::connect("127.0.0.1:8080").await?;
+
+    minase.query(
+        minase::db_core::query::Query::SelectTable {
+            table: 0,
+        }
+    ).await?;
+    let _ = minase.receive_table().await;
+
+    minase.query(
+        minase::db_core::query::Query::Exit
+    ).await?;
+
+    Ok(())
 }
